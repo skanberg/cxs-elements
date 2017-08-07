2ee2c1b7e84e2a9478591d78e4797e8ff8e8d5c4
Fredrik Skanberg <skanberg@gmail.com>
2017-08-07 13:57:29 +0200

    Added table to example.

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,25 +1,53 @@
 import React from "react";
 import { render } from "react-dom";
-import { Div, Span, FlexRow, FlexColumn, A } from "../../src";
+import {
+  Div,
+  Span,
+  FlexRow,
+  FlexColumn,
+  A,
+  Table,
+  TableRow,
+  TableCell,
+} from "../../src";
 
 const Demo = () =>
   <div>
-    <Div color="blue" fontSize={20}>This is a div with some style</Div>
+    <Div color="blue" fontSize={20}>
+      This is a div with some style
+    </Div>
     <Div marginTop={10}>
       This is a <Span color="red">div</Span> with some{" "}
       <Span fontStyle="italic">spans</Span> inline
     </Div>
     <FlexRow marginTop={20}>
-      <Div fontColor="cyan" marginRight={20}>This the first element in a flex row</Div>
-      <Div>And <Span fontWeight="bold">another</Span> element</Div>
+      <Div fontColor="cyan" marginRight={20}>
+        This the first element in a flex row
+      </Div>
+      <Div>
+        And <Span fontWeight="bold">another</Span> element
+      </Div>
     </FlexRow>
     <FlexColumn marginTop={20}>
       <div>First element in a flex column</div>
       <div>Second element</div>
     </FlexColumn>
     <Div marginTop={20}>
-      This is a <A fontWeight="bold" href="https://github.com/" target="_blank">link</A>
+      This is a{" "}
+      <A fontWeight="bold" href="https://github.com/" target="_blank">
+        link
+      </A>
     </Div>
+    <Table marginTop={20}>
+      <TableRow>
+        <TableCell padding={5} border="1px solid blue">
+          This is a cell in a table
+        </TableCell>
+        <TableCell padding={5} border="1px solid red">
+          And this is another cell
+        </TableCell>
+      </TableRow>
+    </Table>
   </div>;
 
 render(<Demo />, document.querySelector("#demo"));
